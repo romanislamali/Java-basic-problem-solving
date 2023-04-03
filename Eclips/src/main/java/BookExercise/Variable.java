67575247332a946b4f7854a9595f48c14e89b2ce
Md. Roman Islam Ali <romanislam897@gmail.com>
2023-04-03 21:16:05 +0600

    variable

---
+++ Eclips/src/main/java/BookExercise/Variable.java
@@ -0,0 +1,25 @@
+package BookExercise;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		
+		int y = 1;
+		double radius = 1.0;
+		int x = 5 * (3/2);
+		
+		double a = 3 / 2;
+		
+		double b = 3 % 2;
+		
+		double d = 5 * (3/2);
+		double area = radius * radius * 3.14159;
+
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(area);
+		System.out.println(d);
+
+	}
+
+}
