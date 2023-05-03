ba0935207102e45aecb6d778f2667e8b82e8cba1
Md. Roman Islam Ali <romanislam897@gmail.com>
2023-05-03 21:03:17 +0600

    Update

---
+++ Eclips/src/main/java/PrimeNumber.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+		
+		System.out.print("Write a number: ");
+		Scanner sc = new Scanner(System.in);
+		
+		int input = sc.nextInt();
+		
+		int count = 0;
+		for(int i = 1; i <= input; i++) {
+			if(input % i == 0) {
+				count++;
+			}
+		}
+		
+		if(count == 2) {
+			System.out.println(input+" is a prime number!!");
+		}else {
+			System.out.println(input+" is not a prime number!!");
+		}
+
+	}
+
+}
