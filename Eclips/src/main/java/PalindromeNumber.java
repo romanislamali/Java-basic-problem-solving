625448217cd1ab6fd40a0ead5aacede08685ef4e
Md. Roman Islam Ali <romanislam897@gmail.com>
2023-05-05 12:19:10 +0600

    Palindrome String, Palindrome number Binary search

---
+++ Eclips/src/main/java/PalindromeNumber.java
@@ -0,0 +1,22 @@
+
+public class PalindromeNumber {
+
+	public static void main(String[] args) {
+
+		int riminder, sum = 0, temp;    
+		int number = 454;//It is the number variable to be checked for palindrome  
+
+		temp = number;    
+		while(number > 0){    
+			riminder = number % 10;  //getting remainder  
+			sum = (sum * 10) + riminder;    
+			number = number/10;    
+		}    
+		if(temp == sum)    
+			System.out.println("palindrome number ");    
+		else    
+			System.out.println("not palindrome");    
+
+	}
+
+}
