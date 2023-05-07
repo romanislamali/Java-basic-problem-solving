258796c6f0587d93b3c9c15fa6f3f5d589b314e7
Md. Roman Islam Ali <romanislam897@gmail.com>
2023-05-07 20:45:18 +0600

    Store value array to array

--- Eclips/src/main/java/StoreValueArrayToArray.java
+++ Eclips/src/main/java/StoreValueArrayToArray.java
@@ -3,22 +3,28 @@ public class StoreValueArrayToArray {
 
 	public static void main(String[] args) {
 
-		int a[] = {10,20,30,40,50};
-
+		int a[] = {10,20,30,40,50,60,70,80,90,100};
+		
+		//print a[] array
+		System.out.print("Array a[] = ");
+		for(int x : a) {
+			System.out.print(x+" ");
+		}		
+		
+		//declare b[] array with same length of a[]
 		int b[] = new int[a.length];
 		
-		for(int x : a){
-			System.out.print(x+" ");
-			for(int i = 0; i <= a.length-1; i++) {
-				b[i] = x;
-			}
+		//storing value of a[] to b[]
+		for(int i = 0; i < a.length; i++) {
+			b[i] = a[i];			
 		}
 		
+		//print b[] array
 		System.out.println();
-		for(int y : b){
+		System.out.print("Array b[] = ");
+		for(int y : b) {
 			System.out.print(y+" ");
 		}
-//		System.out.println(b[5]);
 	}
 
 }
