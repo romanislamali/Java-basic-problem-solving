f7fe5da0294fa095d12fb21c098ee783690ffef0
Md. Roman Islam Ali <romanislam897@gmail.com>
2023-05-08 00:25:16 +0600

    Two dimensional array

---
+++ Eclips/src/main/java/TwoDiamensionalArray.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class TwoDiamensionalArray {
+
+	public static void main(String[] args) {
+		
+		int x = a[3][3];
+		int n;
+		
+		
+		for ( int i=0; i<n ;i++) {		  
+		    for (int j=0; j<n; j++){   		      
+		        a[3][3]= 0;   
+		    }  
+		}
+	}
+}
