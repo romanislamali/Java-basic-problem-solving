519ede489d7c8689df5cce0b4169676d07a4d0a4
Md. Roman Islam Ali <romanislam897@gmail.com>
2023-05-14 20:05:11 +0600

    update

---
+++ Eclips/src/main/java/SwappingTwoVariableExceptThirdVariable.java
@@ -0,0 +1,18 @@
+
+public class SwappingTwoVariableExceptThirdVariable {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		int b = 20;
+		
+		b = a + b;
+		a = b - a;
+		System.out.println("a = "+a);
+		
+		b = b - a;
+		System.out.println("b = "+b);
+
+	}
+
+}
